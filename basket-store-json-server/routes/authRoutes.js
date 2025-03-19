731715004b828cfb91f978693e3299b46049e2e9
Pedro Tauzen <pedrotauzen@gmail.com>
2025-03-19 20:27:12 +0000

    Ajuste para logout

--- basket-store-json-server/routes/authRoutes.js
+++ basket-store-json-server/routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const verifyToken = require('../middleware/verifyToken');
 
 // Rota para login
 router.post('/login', authController.login);
+
+// Rota para logout
+router.post('/logout', verifyToken, authController.logout);
 
 module.exports = router;
